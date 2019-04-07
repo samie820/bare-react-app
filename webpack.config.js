a99cd33b9c6041b471899657516d86249867f011
samie820 <samuelomole70@gmail.com>
2019-04-07 22:41:25 +0100

    Fixed all the security issues and updated the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,171 @@
-const path = require("path");
+const debug = process.env.NODE_ENV !== "production";
 const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  output: {
+    publicPath: "/",
+    path: path.join(__dirname, "build"),
+    filename: "js/[name].bundle.min.js",
+    chunkFilename: "js/[name].bundle.js"
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./dev",
+    port: 2500,
+    historyApiFallback: true
+  },
+  devtool: debug ? "cheap-module-eval-source-map" : false,
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        query: {
+          presets: ["@babel/env", "@babel/preset-react"],
+          plugins: [
+            "react-html-attrs",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-syntax-dynamic-import",
+            "@babel/plugin-proposal-object-rest-spread",
+            ["@babel/plugin-proposal-decorators", { legacy: true }]
+          ]
+        }
+      },
+      {
+        test: /\.scss/,
+        use: debug
+          ? [
+              {
+                loader: "style-loader"
+              },
+              {
+                loader: "css-loader"
+              },
+              {
+                loader: "sass-loader"
+              }
+            ]
+          : [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+            ]
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: debug
+          ? [
+              {
+                loader: "style-loader"
+              },
+              {
+                loader: "css-loader"
+              },
+              {
+                loader: "sass-loader"
+              }
+            ]
+          : [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+            ]
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/,
+        test: /\.(eot|ttf|woff|woff2|otf|svg)$/,
         use: [
           {
-            loader: 'file-loader',
-            options: {}
+            loader: "url-loader",
+            options: {
+              limit: 100000,
+              name: "./assets/fonts/[name].[ext]"
+              // publicPath: '../'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/images/"
+            }
           }
         ]
       }
     ]
   },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3500,
-    publicPath: "http://localhost:3500/dist/",
-    hot: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: debug
+    ? [
+        new CircularDependencyPlugin({
+          // exclude detection of files based on a RegExp
+          exclude: /a\.js|node_modules/,
+          // add errors to webpack instead of warnings
+          failOnError: true,
+          // set the current working directory for displaying module paths
+          cwd: process.cwd()
+        }),
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+        })
+      ]
+    : [
+        // define NODE_ENV to remove unnecessary code
+        new webpack.DefinePlugin({
+          "process.env.NODE_ENV": JSON.stringify("production")
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(), // Merge chunks
+        // extract imported css into own file
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: "[name].css",
+          chunkFilename: "[id].css"
+        }),
+        new webpack.LoaderOptionsPlugin({
+          minimize: true
+        }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new HtmlWebpackPlugin({
+          template: "./src/index.html"
+          // minify: {
+          //   collapseWhitespace: true,
+          //   removeAttributeQuotes: false
+          // }
+        }),
+        new CompressionPlugin({
+          test: /\.(html|css|js|gif|svg|ico|woff|ttf|eot)$/,
+          exclude: /(node_modules)/
+        }),
+        new BundleAnalyzerPlugin()
+      ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          ie8: true,
+          safari10: true,
+          sourceMap: true
+        }
+      })
+    ]
+  }
 };
